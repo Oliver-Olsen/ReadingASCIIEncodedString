d5022946a0e7760c94b91ec9f1b2847d5eec0fac
Metafysical <wulff43211234@gmail.com>
2025-01-08 11:09:20 +0100

    Added pin def and setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,24 @@
-#include <Arduino.h>
+#include<Arduino.h>
 
+#define LED_RED 11
+#define LED_GREEN 12
+#define LED_BLUE 13
+
+
+int pins[3] = {
+  LED_RED, 
+  LED_GREEN, 
+  LED_BLUE
+}; 
 
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600); 
+
+  for (int i = 0; i < 3; i++) {
+    pinMode(pins[i], OUTPUT); 
+    digitalWrite(pins[i], LOW); 
+  }
 }
 
 void loop() {
