b7e090a23742d4035c6809ca8784dcb345851bcc
Metafysical <wulff43211234@gmail.com>
2025-01-08 11:35:17 +0100

    Fixed code

--- src/main.cpp
+++ src/main.cpp
@@ -6,17 +6,12 @@
 *   We use 8 bytes to control our colour values. With 8 bytes we can write 256 number (0-255).
 *   This is range, is for each of the 3 colors (red, greenand blue). So there is 256*256*256 different combinations.
 *
-*   7b. THe Serial.parseInt(). It skips everything that is not an int or a minus.
+*   7b. The Serial.parseInt(). It skips everything that is not an int or a minus.
 *   It returns the next valid int.
 */
 
 #include <Arduino.h>
 
-int incomingByte = 0;
-
-
-  
-  
 #define LED_RED 11
 #define LED_GREEN 12
 #define LED_BLUE 13
@@ -30,7 +25,7 @@ int pins[3] = {
 
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600); 
 
   for (int i = 0; i < 3; i++) {
     pinMode(pins[i], OUTPUT); 
@@ -39,11 +34,12 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   if (Serial.available() > 0) {
-    incomingByte = Serial.read();
-    incomingByte = Serial.parseInt();
-    Serial.print(incomingByte);
+    for (int i = 0; 0 < Serial.available(); i++) {
+      int k = Serial.read(); 
+      k = Serial.parseInt(); 
+      analogWrite(pins[i], k); 
+    }
+  }
 }
-}
-
+ 
